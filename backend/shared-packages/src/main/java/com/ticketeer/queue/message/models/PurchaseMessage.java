ffeeb0b655feb32e775505ccc1590982243db48c
m001653 <einstein.bata@mozabanco.co.mz>
2026-05-13 15:54:46 +0200

    Change the project structure to a common parent pom.xml

---
+++ backend/shared-packages/src/main/java/com/ticketeer/queue/message/models/PurchaseMessage.java
@@ -0,0 +1,14 @@
+package com.ticketeer.queue.message.models;
+
+public class PurchaseMessage {
+    private String testing;
+
+    public String getTesting() {
+        return testing;
+    }
+
+    public void setTesting(String testing) {
+        this.testing = testing;
+    }
+
+}
